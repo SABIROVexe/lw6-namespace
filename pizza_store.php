3369772431e7a550ae5460cc131942f7ff450641
SABIROVexe <sabirov.main@gmail.com>
2025-09-21 04:09:04 +0300

    доработал класс Pizza

--- pizza_store.php
+++ pizza_store.php
@@ -2,16 +2,29 @@
 
 class PizzaStore{
 
+     function orderPizza(string $type){
+        function createPizza(string $type){
+            echo("Тип пиццы: " . $type);
+        }
+     }
+
+
 }
 
 class Pizza{
     public $name, $sauce;
     public $toppings = [];
 
-    public function prepare(){
-        echo("Началась готовка пиццы:" . $name) ;
-        echo("Добавлен соус:" . $sauce);
-        echo("Добавлены топики:") ;
+    public function __construct($name, $sauce, $toppings) {
+        $this->name = $name;
+        $this->sauce = $sauce;
+        $this->toppings = $toppings;
+    }
+
+    public function prepare() {
+        echo ("Началась готовка пиццы: " . $this->name . "\n");
+        echo ("Добавлен соус: " . $this->sauce . "\n");
+        echo ("Добавлены топики: " . implode(", ", $this->toppings) . "\n");
     }
 
     public function cut(){
